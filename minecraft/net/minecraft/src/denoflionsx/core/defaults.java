78d7f466947d07c3e77584ce5be62c0ce645eab4
den <denvstheworld@gmail.com>
2012-07-25 08:26:13 -0400

    Fixed Wooden bucket working in the nether. Added config option to keep bugged wooden bucket behavior.

--- minecraft/net/minecraft/src/denoflionsx/core/defaults.java
+++ minecraft/net/minecraft/src/denoflionsx/core/defaults.java
@@ -16,6 +16,7 @@ public class defaults {
         core.config.addDefault("EnableBC2TankIntegration=false");
         core.config.addDefault("Verbose=true");
         core.config.addDefault("UpdateCheck=true");
+        core.config.addDefault("WoodenBucketWorksInNether=false");
         core.config.addDefault("liquidVacuum_ID=" + String.valueOf(5312 + 3));
         core.config.writeConfig();
     }
