0eb8da6d48cd134edd53a088c0e5e392c758b0ab
den <denvstheworld@gmail.com>
2013-02-25 18:47:17 -0500

    Turn off cBuild flag.

--- src/denoflionsx/PluginsforForestry/Config/BuildFlags.java
+++ src/denoflionsx/PluginsforForestry/Config/BuildFlags.java
@@ -2,6 +2,6 @@ package denoflionsx.PluginsforForestry.Config;
 
 public class BuildFlags {
     
-    public static final boolean isCustomBuild = true;
+    public static final boolean isCustomBuild = false;
     
 }
