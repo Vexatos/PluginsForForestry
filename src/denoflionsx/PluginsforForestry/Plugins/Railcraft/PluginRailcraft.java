7625089e76d9514c12074f79b3feda6b9ef6abf9
den <denvstheworld@gmail.com>
2013-05-25 18:34:55 -0400

    Proper load wrap.

--- src/denoflionsx/PluginsforForestry/Plugins/Railcraft/PluginRailcraft.java
+++ src/denoflionsx/PluginsforForestry/Plugins/Railcraft/PluginRailcraft.java
@@ -1,5 +1,6 @@
 package denoflionsx.PluginsforForestry.Plugins.Railcraft;
 
+import cpw.mods.fml.common.Loader;
 import denoflionsx.PluginsforForestry.API.Plugin.IPfFPlugin;
 import denoflionsx.PluginsforForestry.Config.PfFTuning;
 import denoflionsx.PluginsforForestry.Core.PfF;
@@ -7,18 +8,24 @@ import denoflionsx.PluginsforForestry.Plugins.Railcraft.Items.ItemCustomCoke;
 import denoflionsx.PluginsforForestry.Plugins.Railcraft.Items.RCItems;
 
 public class PluginRailcraft implements IPfFPlugin {
-    
+
     @Override
     public void onPreLoad() {
+        if (Loader.isModLoaded("Railcraft")) {
+        }
     }
-    
+
     @Override
     public void onLoad() {
-        RCItems.itemCharCoke = new ItemCustomCoke(PfFTuning.getInt(PfFTuning.Railcraft.plugin_railcraft_charcoal));
-        PfF.Proxy.ItemCollections.add(RCItems.class);
+        if (Loader.isModLoaded("Railcraft")) {
+            RCItems.itemCharCoke = new ItemCustomCoke(PfFTuning.getInt(PfFTuning.Railcraft.plugin_railcraft_charcoal));
+            PfF.Proxy.ItemCollections.add(RCItems.class);
+        }
     }
-    
+
     @Override
     public void onPostLoad() {
+        if (Loader.isModLoaded("Railcraft")) {
+        }
     }
 }
