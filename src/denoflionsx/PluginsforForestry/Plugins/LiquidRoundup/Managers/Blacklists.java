31ff11df4a57dc6d209af66af9fa80013ff2c87d
den <denvstheworld@gmail.com>
2013-05-27 20:18:50 -0400

    blacklist BC liquids in iron buckets.

--- src/denoflionsx/PluginsforForestry/Plugins/LiquidRoundup/Managers/Blacklists.java
+++ src/denoflionsx/PluginsforForestry/Plugins/LiquidRoundup/Managers/Blacklists.java
@@ -6,7 +6,7 @@ public class Blacklists {
      * Due to mostly load order issues, LR will sometimes create duplicates.
      * So this blacklist prevents that.
      */
-    public static final String[] ironBucket = new String[]{"Creosote Oil", "redstone", "ender", "Steam", "immibis.liquidxp"};
+    public static final String[] ironBucket = new String[]{"Creosote Oil", "redstone", "ender", "Steam", "immibis.liquidxp", "Oil", "Fuel"};
     public static final String[] capsule = new String[]{"Oil", "Fuel", "Steam", "Creosote Oil"};
     public static final String[] woodenBucket = new String[]{"Steam", "Lava"};
     public static final String[] barrel = new String[]{"Steam"};
