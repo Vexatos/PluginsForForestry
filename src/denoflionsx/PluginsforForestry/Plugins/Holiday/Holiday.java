22d05ded8c53cfbd344004b924e2022db53676ed
denoflions <denvstheworld@gmail.com>
2013-11-01 21:41:10 -0400

    Added Christmas.

--- src/denoflionsx/PluginsforForestry/Plugins/Holiday/Holiday.java
+++ src/denoflionsx/PluginsforForestry/Plugins/Holiday/Holiday.java
@@ -9,7 +9,8 @@ public class Holiday implements IPfFPlugin {
 
     private HolidayObject[] days = new HolidayObject[]{
         new HolidayObject("Halloween", new DateObject(Calendar.OCTOBER).setWholeMonth(true), "holiday/wooden_bucket_spooky", "item.pff.woodenbucket.halloween.name"),
-        new HolidayObject("Thanksgiving (US)", new DateObject(Calendar.NOVEMBER, 21, 28), "holiday/pilgrim_bucket", "item.pff.woodenbucket.thanksgiving.name")
+        new HolidayObject("Thanksgiving (US)", new DateObject(Calendar.NOVEMBER, 21, 28), "holiday/pilgrim_bucket", "item.pff.woodenbucket.thanksgiving.name"),
+        new HolidayObject("Christmas", new DateObject(Calendar.DECEMBER).setWholeMonth(true), "holiday/christmas_bucket", "item.pff.woodenbucket.christmas.name")
     };
 
     @Override
